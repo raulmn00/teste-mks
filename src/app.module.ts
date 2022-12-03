91f321d769b4478d5d8da8bfbc048b3217834c17
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-12-03 11:04:59 -0300

    feat: config typeorm module in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'containers-us-west-125.railway.app',
+      port: 6098,
+      database: 'railway',
+      username: 'postgres',
+      password: 'IxnFxrJDYiKPEiMKmmaP',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js, .ts}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
