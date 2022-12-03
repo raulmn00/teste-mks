7657bf00f8a74faafc49a5d374ef9ebc29e3c23d
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-12-03 11:31:40 -0300

    feat: create user repository and inject it as dependency in user module and user service

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,7 +1,35 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'users' })
-export class UsersEntity {
+export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'user_name', nullable: false })
+  userName: string;
+
+  @Column({ name: 'user_email', nullable: false })
+  userEmail: string;
+
+  @Column({ name: 'user_password', nullable: false })
+  userPassword: string;
+
+  @Column({ name: 'user_cpf', nullable: false })
+  userCpf: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
 
 @Module({
-  providers: [UserService]
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserService],
 })
 export class UserModule {}
