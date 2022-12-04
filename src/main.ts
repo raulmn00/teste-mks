969d2a004a61b24154538b07fb6929def1f9ac45
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-12-03 22:00:22 -0300

    feat: install and implement swagger and yours dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('API Teste MKS')
+    .setDescription('API para o teste técnico da MKS.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -7,29 +7,46 @@ import {
   DeleteDateColumn,
 } from 'typeorm';
 
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from '@nestjs/class-validator';
+
 @Entity({ name: 'users' })
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ name: 'user_name', nullable: false })
+  @ApiProperty()
+  @IsString()
   userName: string;
 
   @Column({ name: 'user_email', nullable: false })
+  @ApiProperty()
+  @IsString()
   userEmail: string;
 
   @Column({ name: 'user_password', nullable: false })
+  @ApiProperty()
+  @IsString()
   userPassword: string;
 
   @Column({ name: 'user_cpf', nullable: false })
+  @ApiProperty()
+  @IsString()
   userCpf: string;
 
   @CreateDateColumn({ name: 'created_at' })
+  @ApiProperty()
+  @IsString()
   createdAt: string;
 
   @UpdateDateColumn({ name: 'updated_at' })
+  @ApiProperty()
+  @IsString()
   updatedAt: string;
 
   @DeleteDateColumn({ name: 'deleted_at' })
+  @ApiProperty()
+  @IsString()
   deletedAt: string;
 }
